af6c06086d18dc9af7fc45505c227b8b3339e691
Jed Boyle <jedboyle01@gmail.com>
2020-05-12 19:51:22 +0100

    004 - Create a control flow statement that checks whether the runner is an adult AND registered early. Add 1000 to their raceNumber if this is true.

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// 001 Assign race numbers randomly.
+// 001 - Assign race numbers randomly.
 let raceNumber = Math.floor(Math.random() * 1000);
 
-// 002 Create a variable that indicates whether a runner registered early or not.
+// 002 - Create a variable that indicates whether a runner registered early or not.
 let early = true;
 
-// 003 Create a variable for the runner’s age and set it equal to a number.
+// 003 - Create a variable for the runner’s age and set it equal to a number.
+let age = 19;
 
+// 004 - Create a control flow statement that checks whether the runner is an adult AND registered early. Add 1000 to their raceNumber if this is true.
+if (early && age > 18) {
+    raceNumber += 1000;
+};
