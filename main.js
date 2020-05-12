35892a6597efc6097c238fa519efb3fcb836c42a
Jed Boyle <jedboyle01@gmail.com>
2020-05-12 19:46:18 +0100

    002 Create a variable that indicates whether a runner registered early or not.

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-// 001 Assign race numbers randomly
+// 001 Assign race numbers randomly.
 let raceNumber = Math.floor(Math.random() * 1000);
+
+// 002 Create a variable that indicates whether a runner registered early or not.
+let early = true;
+
