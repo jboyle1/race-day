431ac844eaaac49d573096e288cdb0f0f6038bfc
Jed Boyle <jedboyle01@gmail.com>
2020-05-12 19:47:08 +0100

    Create a variable for the runner’s age and set it equal to a number.

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ let raceNumber = Math.floor(Math.random() * 1000);
 // 002 Create a variable that indicates whether a runner registered early or not.
 let early = true;
 
+// 003 Create a variable for the runner’s age and set it equal to a number.
+
